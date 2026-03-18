7dc526f50f963bc7ce5aaaf5b30a50097ad76f02
Maria <a97304@alunos.uminho.pt>
2026-03-18 17:00:19 +0000

    codigo da bea

---
+++ testes.py
@@ -0,0 +1,28 @@
+#para teste
+registo1 = [{"data": '10-11',
+                "area": 120,
+                "perimetro": 120,
+                "dor": 8,
+                "febre": True,
+                "evolucao": 'negativa',
+                "risco": 'elevado'},
+            {"data": '11-11',
+                "area": 120,
+                "perimetro": 120,
+                "dor":9,
+                "febre": True,
+                "evolucao": 'positiva',
+                "risco": 'baixo'},
+            {"data": '12-11',
+                "area": 120,
+                "perimetro": 120,
+                "dor":10,
+                "febre": False,
+                "evolucao": 'positiva',
+                "risco": 'baixo'}]
+
+#print(pLista(registo1, "data"))
+#print(gerar_relatorio_geral(registo1))
+#python -m uvicorn main:app --reload
+#cd 'C:\Users\Utilizador\Desktop\projeto'
+#python -m streamlit run app.py
